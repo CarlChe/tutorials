4892670ae43aa47d9076285f77730893388553ca
Carl Che <carl.che.te@renesas.com>
2024-11-25 22:49:12 +0800

    Adding learning notes for lesson 102

--- lessons/102/terraform/0-provider.tf
+++ lessons/102/terraform/0-provider.tf
@@ -9,4 +9,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-on-aws-eks-state"
+    key    = "devops/terraform.tfstate"
+    region = "us-east-1"
+
+    # Enable during Step-09     
+    # For State Locking
+    dynamodb_table = "devops-dynamo-lock-table"
+  }
 }
